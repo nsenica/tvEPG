146834285bb16133b282a8ad7fd18f10505127dd
nsenica <nsenica@gmail.com>
2026-06-09 15:45:16 +0100

    Fix MEO GO provider + rewrite Vodafone provider
    - pt_meo_go: switch to the current meo (meogouser.apps.meo.pt) API; the
      old www.meo.pt grid endpoint stopped returning data (0 programmes).
    - pt_vodafone: rewrite for the public Kaltura EPG API
      (cdn.pt.vtv.vodafone.com/epg/{site_id}/{Y}/{M}/{D}/{slot}); the old
      web.ott-red.vodafone.pt endpoint is dead. Serves as a reliable
      fallback source for premium channels.
    - pt_meo / pt_nos: assorted provider fixes.
    - requirements.txt: pin beautifulsoup4, bs4, pytz, soupsieve.

--- providers/pt_meo_go.py
+++ providers/pt_meo_go.py
@@ -7,63 +7,77 @@ import logging
 import json
 import re
 import ssl
-from datetime import datetime,timedelta
+import pytz
+from datetime import datetime, timedelta
 from classes.xmltv.Channel import Channel
 from classes.xmltv.Programme import Programme
 from classes.xmltv.XMLTV import XMLTV
 
-# Implements basic client for https://www.meo.pt/tv/canais-servicos-tv/guia-tv
-# List of channels (POST): https://www.meo.pt/_layouts/15/Ptsi.Isites.GridTv/GridTvMng.asmx/getGridAnon
-baseUrl = "https://www.meo.pt/tv/canais-servicos-tv/guia-tv"
+# Implements client for meogouser.apps.meo.pt APIs
+baseUrl = "https://www.meo.pt"
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 def getEPG(list, nr_days):
-
-    #get current and next day
-    today= datetime.now()
-    sDate=str(today.year)+"-"+str(today.month).zfill(2)+"-"+str(today.day).zfill(2)
-    tomorrow=today+timedelta(days=nr_days)
-    eDate=str(tomorrow.year)+"-"+str(tomorrow.month).zfill(2)+"-"+str(tomorrow.day).zfill(2)
-
     xmltv = XMLTV()
     supportedChannels = _getSupportedChannels()
 
+    tz = pytz.timezone("Europe/Lisbon")
+
     for item in list:
-
-        if (supportedChannels != None and item.getProviderCode() in supportedChannels):
-
+        provider_code = item.getProviderCode()
+        if supportedChannels is not None and provider_code in supportedChannels:
             for channelInfo in item.getChannelList():
                 channel = Channel(channelInfo.getId(), channelInfo.getDisplayName(), "pt", channelInfo.getIconSrc())
                 channel.setUrl(baseUrl)
                 xmltv.addChannel(channel)
                 logging.debug("[MEOGO] Adding %s (%s)", channelInfo.getId(), channelInfo.getDisplayName())
-
         else:
-            logging.warning("[MEOGO] %s - Channel id not available." % format(item.getProviderCode()))
+            logging.warning("[MEOGO] %s - Channel id not available." % format(provider_code))
             continue
 
-        programIds = _getProgramIdsForChannel(item.getProviderCode(), sDate, eDate)
-
-        for pid in programIds:
-
-            try:           
-                d = _getProgramDetailsById(pid)
-                if d is None:
-                    continue
-            except TypeError:
+        # Fetch programs day-by-day
+        today = datetime.now(tz)
+        for i in range(nr_days):
+            current_day = today + timedelta(days=i)
+            date_str = current_day.strftime("%Y-%m-%d")
+            
+            programs_data = _getProgramsForChannel(provider_code, date_str)
+            if not programs_data:
                 continue
+            
+            for prog in programs_data:
+                try:
+                    sTime_naive = datetime.strptime(prog["StartDate"], "%Y-%m-%dT%H:%M:%S")
+                    eTime_naive = datetime.strptime(prog["EndDate"], "%Y-%m-%dT%H:%M:%S")
+                    
+                    sTime_loc = tz.localize(sTime_naive)
+                    eTime_loc = tz.localize(eTime_naive)
+                    
+                    sTime = sTime_loc.strftime("%Y%m%d%H%M%S %z")
+                    eTime = eTime_loc.strftime("%Y%m%d%H%M%S %z")
+                except Exception as e:
+                    logging.error("[MEOGO] Error parsing program date: %s", e)
+                    continue
 
-            for channelInfo in item.getChannelList():
-                p = Programme(channelInfo.getId(), d["sTime"], d["eTime"], d["title"], d["desc"], "pt", d["icon"])
-                _findSeasonEpisode(d["title"],p)
-                xmltv.addProgramme(p)
-                logging.debug("[MEOGO] Programme for %s (%s - %s) added", channelInfo.getId(), d["sTime"], d["eTime"])
+                title = prog.get("Title") or "Sem título"
+                desc = prog.get("Synopsis") or ""
+                
+                # Construct image URL using program title
+                icon = None
+                if title:
+                    encodedTitle = urllib.parse.quote(title)
+                    icon = f"https://cdn-er-images.online.meo.pt/eemstb/ImageHandler.ashx?chCallLetter={provider_code}&progTitle={encodedTitle}&profile=16_9&profileFallback=false&noFallback=true&appSource=PC_CHROME_PWA&width=1920&csf"
 
+                for channelInfo in item.getChannelList():
+                    p = Programme(channelInfo.getId(), sTime, eTime, title, desc, "pt", icon)
+                    _findSeasonEpisode(title, p)
+                    xmltv.addProgramme(p)
+                    logging.debug("[MEOGO] Programme for %s (%s - %s) added", channelInfo.getId(), sTime, eTime)
 
     return xmltv
 
-def _findSeasonEpisode(title,p):
+def _findSeasonEpisode(title, p):
     m = re.match(r'.*(\s+|:)T(\d+)(?:\s+-?\s*Ep.\s*)?(\d+)?$', title)
     if m:
         season = m.group(2)
@@ -79,90 +93,40 @@ def _findSeasonEpisode(title,p):
             if episode:
                 p.setEpisodeNumber(episode)
 
+def _getContents(url):
+    req = urllib.request.Request(
+        url,
+        headers={
+            'Origin': 'https://www.meo.pt',
+            'Referer': 'https://www.meo.pt/',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Mobile Safari/537.36',
+            'Accept': '*/*'
+        }
+    )
+    try:
+        content = urllib.request.urlopen(req, timeout=10)
+        if content.getcode() != 200:
+            return None
+        myfile = content.read().decode('utf-8')
+        return json.loads(myfile)
+    except Exception as e:
+        logging.error("[MEOGO] HTTP request failed: %s", e)
+        return None
+
 def _getSupportedChannels():
-
-    myfile = _getContents("https://www.meo.pt/_layouts/15/Ptsi.Isites.GridTv/GridTvMng.asmx/getGridAnon",{"service":"allchannels"})
+    url = "https://meogouser.apps.meo.pt/Services/GridTv/GridTv.svc/GetContentsForChannels?userAgent=IPTV_OFR_GTV"
+    data = _getContents(url)
     channels = set()
-
-    if myfile is None:
+    if data is None or data.get("Status") != "OK":
         return channels
-
-    for channel in myfile["d"]["channels"]:
-        channels.add(channel["sigla"])
-
+    for channel in data.get("Result", []):
+        if channel.get("CallLetter"):
+            channels.add(channel["CallLetter"])
     return channels
 
-def _getProgramIdsForChannel(sigla, sDate, eDate):
-
-    payload = {"service":"channelsguide", "dateStart": sDate+"T00:00:00.000Z", "dateEnd": eDate+"T23:59:59.000Z","accountID":"", "channels": [( sigla )] }
-    myfile = _getContents("https://www.meo.pt/_layouts/15/Ptsi.Isites.GridTv/GridTvMng.asmx/getProgramsFromChannels",payload)
-    programs = list()
-
-    if myfile is None:
-        return programs
-
-    for channel in myfile["d"]["channels"]:
-        for program in channel["programs"]:
-            programs.append(program["uniqueId"])
-
-    return programs
-
-def _getProgramDetailsById(progId):
-
-    myfile = _getContents("https://www.meo.pt/_layouts/15/Ptsi.Isites.GridTv/GridTvMng.asmx/getProgramDetails",{"service":"programdetail","accountID":"", "programID": progId })
-    
-    if myfile is None:
-        return None
-
-    info = myfile["d"]
-    
-    dstOffset = " +0000"
-    if time.localtime( ).tm_isdst: dstOffset = " +0100"
-
-    date = info["date"].split('-')
-    sTime = info["startTime"]
-    eTime = info["endTime"]
-
-    sTime = datetime(int(date[2]), int(date[1]), int(date[0]), int(sTime.split(":")[0]), int(sTime.split(":")[1]), 0)
-    eTime = datetime(int(date[2]), int(date[1]), int(date[0]), int(eTime.split(":")[0]), int(eTime.split(":")[1]), 0)
-
-
-    if sTime >= datetime(2019,3,31,1,0,0) and sTime <= datetime(2019,3,31,4,0,0):
-        logging.info("[MEOGO] Skipping due to erroneous datetime handling during DST transition...")
-        return None
-
-    if eTime >= datetime(2019,3,31,1,0,0) and eTime <= datetime(2019,3,31,4,0,0):
-        logging.info("[MEOGO] Skipping due to erroneous datetime handling during DST transition...")
-        return None
-    
-    if (eTime < sTime):
-        eTime += timedelta(days=1)
-
-    sTime = sTime.strftime("%Y%m%d%H%M%S") + dstOffset
-    eTime = eTime.strftime("%Y%m%d%H%M%S") + dstOffset
-
-    logging.debug("[MEOGO] %s : Got program details for %s", info["date"], progId)
-
-    return { "sTime": sTime , "eTime": eTime , "icon": info["progImageXL"], "title" : info["progName"], "desc" : info["description"] }
-
-def _getContents(url,payload):
-
-    data = json.dumps(payload).encode('utf-8')
-    handler = urllib.request.HTTPSHandler(debuglevel=0)
-    opener = urllib.request.build_opener(handler)
-    request = urllib.request.Request(url, data=data, headers={'Content-type': 'application/json; charset=UTF-8', 'Accept': '*/*'})
-    content = opener.open(request, timeout = 10)
-
-    if content.getcode() != 200: return None
-    try:
-        myfile = content.read()
-        myfile = myfile.decode('utf8')
-        myfile = json.loads(myfile)
-    except ValueError:
-        logging.error("Error parsing json")
-        return None
-    except:
-        logging.error("Something went wrong here...")
-        return None
-
-    return myfile
+def _getProgramsForChannel(sigla, date_str):
+    url = f"https://meogouser.apps.meo.pt/Services/GridTv/GridTv.svc/GetLiveChannelProgramsByDate?callLetter={urllib.parse.quote(sigla)}&date={date_str}&userAgent=IPTV_OFR_GTV"
+    data = _getContents(url)
+    if data is None or data.get("Status") != "OK":
+        return []
+    return data.get("Result", [])
--- providers/pt_vodafone.py
+++ providers/pt_vodafone.py
@@ -1,357 +1,138 @@
 # -*- coding: utf-8 -*-
 
-# List of channels: https://web.ott-red.vodafone.pt/ott3_webapp/v1/channels
-
-# As of 2018-10-19 (OUTDATED)
-# ChannelId, ChannelName
-# 157,24Kitchen
-# 196,24Kitchen HD
-# 480,AMC
-# 479,AMC HD
-# 47,AXN
-# 45,AXN Black
-# 183,AXN Black HD
-# 108,AXN HD
-# 46,AXN White
-# 184,AXN White HD
-# 345,AfroMusic Portugal
-# 100,Al Jazeera English
-# 488,Angelus TV
-# 499,Arirang TV
-# 341,BBC Entertainment
-# 37,BBC World News
-# 35,Baby TV
-# 230,Benfica TV
-# 231,Benfica TV HD
-# 21,Blaze
-# 508,Blaze HD
-# 59,Bloomberg
-# 507,Boomerang
-# 128,Brava TV HD
-# 34,C Music TV
-# 372,CANAL Q
-# 498,CMTV HD
-# 497,CMTV Portugal
-# 42,CNBC
-# 95,CNN
-# 160,Canal 180
-# 161,Canal 180 HD
-# 16,Canal Parlamento
-# 373,Canal Q HD
-# 202,Canção Nova TV
-# 236,Cartoon Network
-# 241,Caza & Pesca
-# 473,Caçavision
-# 352,Cinemundo
-# 355,Cinemundo SD
-# 101,Cubavision
-# 344,DJAZZ
-# 348,DOGTV
-# 94,Deutche Welle
-# 89,Discovery
-# 387,Discovery HD
-# 48,Disney Channel
-# 193,Disney Junior
-# 412,Docubox HD
-# 52,E!
-# 188,E! HD
-# 511,Eurochannel FR
-# 510,Eurochannel PT
-# 39,Euronews
-# 481,Euronews EN
-# 51,Eurosport
-# 53,Eurosport 2
-# 54,Eurosport HD
-# 18,FOX
-# 105,FOX Comedy
-# 363,FOX Comedy HD
-# 17,FOX Crime
-# 30,Fashion TV
-# 391,Fashion TV HD
-# 351,FastnFunbox HD
-# 374,Fight Network HD
-# 350,Fightbox HD
-# 127,Food Network
-# 476,Food Network HD
-# 199,Fox Crime HD
-# 107,Fox HD
-# 103,Fox Life
-# 104,Fox Life HD
-# 106,Fox Movies
-# 197,Fox Movies HD
-# 81,France 24 Eng
-# 82,France 24 FR
-# 9,Fuel TV
-# 177,Fuel TV HD
-# 125,GINX
-# 15,Globo Now
-# 417,Globo Now HD
-# 20,História
-# 19,Hollywood
-# 478,Hollywood HD
-# 368,KBS
-# 405,Kuriakos TV
-# 194,Localvisão
-# 195,Localvisão HD
-# 62,Luxe TV HD
-# 240,M6
-# 93,MCM POP
-# 92,MCM TOP
-# 356,MCM TOP SD
-# 61,MEZZO
-# 409,MEZZO Live HD
-# 38,MTV Portugal
-# 403,MTV Portugal HD
-# 69,Melody Zen. TV
-# 63,NGC Wild
-# 360,NHK WORLD TV
-# 36,Nat Geo
-# 31,Nat Geo HD
-# 198,Nat Geo Wild HD
-# 73,Nautical Channel
-# 22,Odisseia
-# 490,Odisseia HD
-# 66,PFC
-# 375,PORTOC HD
-# 55,PRO TV International
-# 27,Panda
-# 146,Panda Biggs
-# 76,Phoenix InfoNews
-# 80,Porto Canal
-# 382,QYOU HD
-# 380,QYOU SD
-# 84,RAI 1
-# 85,RAI News
-# 243,RTL
-# 1,RTP 1
-# 171,RTP 1 HD
-# 2,RTP 2
-# 172,RTP 2 HD
-# 3,RTP 3
-# 5,RTP Africa
-# 347,RTP Açores
-# 504,RTP HD
-# 342,RTP Madeira
-# 4,RTP Memoria
-# 110,RTPHD
-# 155,Record News
-# 91,Russia Today
-# 489,Russia Today Documentary
-# 130,SET Max
-# 129,SET Ásia
-# 11,SIC
-# 371,SIC Caras
-# 415,SIC Caras HD
-# 173,SIC H
-# 390,SIC HD
-# 386,SIC K
-# 416,SIC K HD
-# 12,SIC Mulher
-# 414,SIC Mulher HD
-# 13,SIC Notícias
-# 413,SIC Notícias HD
-# 14,SIC Radical
-# 392,SIC Radical HD
-# 6,SPORT TV1
-# 7,SPORT TV2
-# 8,SPORT TV3
-# 154,SPORT TV4
-# 111,SPORT TV5
-# 206,Sky News
-# 407,Sport TV +
-# 408,Sport TV + HD
-# 151,Sport TV 1 HD
-# 152,Sport TV 2 HD
-# 153,Sport TV 3 HD
-# 10,Sport TV 4 HD
-# 112,Sport TV 5 HD
-# 486,Sporting TV
-# 487,Sporting TV HD
-# 144,Star Gold
-# 145,Star Plus
-# 244,Super RTL
-# 228,SyFy HD
-# 227,SyFy SD
-# 485,TCVi
-# 385,TLC
-# 43,TPA Internacional
-# 149,TV Cine 1 HD
-# 150,TV Cine 2 HD
-# 200,TV Galicia
-# 410,TV Globo Básico
-# 501,TV Globo Básico HD
-# 88,TV Record
-# 238,TV Record HD
-# 158,TV Series
-# 159,TV Series HD
-# 33,TV5 Monde
-# 181,TVC 3 HD
-# 182,TVC 4 HD
-# 349,TVC NEWS
-# 23,TVCine 1
-# 24,TVCine 2
-# 25,TVCine 3
-# 26,TVCine 4
-# 41,TVE 24h
-# 40,TVE Internacional
-# 28,TVI
-# 174,TVI HD
-# 113,TVI24
-# 239,Touros
-# 68,Trace TV
-# 495,Trace TV HD
-# 471,Trace Toca
-# 56,Travel
-# 475,Travel HD
-# 50,VH1 Europe
-# 245,VOX
+# Vodafone TV EPG via the public Kaltura "vtv" API.
+#
+#   GET https://cdn.pt.vtv.vodafone.com/epg/{site_id}/{YYYY}/{MM}/{DD}/{slot}
+#
+# Each day is fetched in four 6-hour windows (Europe/Lisbon time): 00-06,
+# 06-12, 12-18, 18-00. The provider "code" in channelList.xml is the Vodafone
+# site_id (e.g. 2687 = Sport TV 1). site_id <-> channel mappings come from:
+#   https://github.com/iptv-org/epg/blob/master/sites/vodafone.pt/vodafone.pt.channels.xml
 
 import urllib.request
-from urllib.error import HTTPError
+from urllib.error import HTTPError, URLError
 import datetime
 import time
 import json
-from collections import defaultdict
+import logging
+import pytz
 from classes.xmltv.Channel import Channel
 from classes.xmltv.Programme import Programme
 from classes.xmltv.XMLTV import XMLTV
-import logging
-import pytz
-import re
 
-baseUrl = "https://web.ott-red.vodafone.pt/"
-url="https://web.ott-red.vodafone.pt/ott3_webapp/v1.5/programs/grids/{0}/{1}"
-iconUrlPrefix = "http://web.ottimg.vodafone.pt/iptvimageserver/Get/{0}_{1}/4_3/684/513"
+API_ENDPOINT = "https://cdn.pt.vtv.vodafone.com/epg"
+SLOTS = ["00-06", "06-12", "12-18", "18-00"]
+HEADERS = {
+    "Origin": "https://www.vodafone.pt",
+    "Referer": "https://www.vodafone.pt/",
+    "User-Agent": "Mozilla/5.0 (compatible; tv_grab_pt_vodafone)",
+    "Accept": "application/json, text/javascript, */*; q=0.01",
+    "Accept-Language": "pt-PT,pt;q=0.9,en;q=0.8",
+}
+
+tz = pytz.timezone("Europe/Lisbon")
+
 
 def getEPG(items, nr_days):
 
-    tz = pytz.timezone("Europe/Lisbon")
-
-    provCodes = defaultdict(list)
     xmltv = XMLTV()
-
-    supportedChannels = _getSupportedChannels()
-    #print(supportedChannels)
+    today = datetime.datetime.now(tz)
 
     for item in items:
 
-        if (supportedChannels != None and item.getProviderCode() in supportedChannels):
-            for channelInfo in item.getChannelList():
+        site_id = item.getProviderCode()
+        channelInfos = item.getChannelList()
 
-                channel = Channel(channelInfo.getId(), channelInfo.getDisplayName(), "pt", channelInfo.getIconSrc())
-                channel.setUrl(baseUrl)
-                xmltv.addChannel(channel)
-                provCodes[item.getProviderCode()].append(channelInfo.getId())
-                logging.debug("[VODAFONE] Adding %s (%s)", channelInfo.getId(), channelInfo.getDisplayName())
-        else:
-            logging.warning("Vodafone: {0} - Channel id not available.".format(item.getProviderCode()))
+        for channelInfo in channelInfos:
+            channel = Channel(channelInfo.getId(), channelInfo.getDisplayName(), "pt", channelInfo.getIconSrc())
+            channel.setUrl("https://www.vodafone.pt/")
+            xmltv.addChannel(channel)
+            logging.debug("[VODAFONE] Adding %s (%s)", channelInfo.getId(), channelInfo.getDisplayName())
 
-        sDate = 0
-        delta = 1
-        eDate = (delta*nr_days-1)
+        seen = set()
+        got = 0
 
-        while sDate <= eDate:
+        for day in range(nr_days):
+            d = today + datetime.timedelta(days=day)
+            for slot in SLOTS:
+                link = "%s/%s/%04d/%02d/%02d/%s" % (API_ENDPOINT, site_id, d.year, d.month, d.day, slot)
+                logging.debug("[VODAFONE] Requesting %s", link)
 
-            link = url.format(urllib.parse.quote(item.getProviderCode()),sDate)
-
-            sDate += delta
-            logging.debug("[VODAFONE] Requesting data from URL %s" % link)
-            
-            content = _get_content(link);
-
-            if content == None or content.getcode() != 200:
-                logging.warning("Couldn't retrieve information for channel. HTTP Error code: %s " % content.getCode() )
-                continue
-            
-            myfile = content.read().decode('utf8')
-            try:
-                myfile = json.loads(myfile)
-            except ValueError:
-                continue
-            for program in myfile["data"]:
-
-                sTime = datetime.datetime.strptime(program["startTime"], "%Y-%m-%dT%H:%M:%SZ")
-                eTime = datetime.datetime.strptime(program["endTime"], "%Y-%m-%dT%H:%M:%SZ")
-
-                if sTime >= datetime.datetime(2019,3,31,1,0,0) and sTime <= datetime.datetime(2019,3,31,4,0,0):
-                    logging.info("[VODAFONE] Skipping due to erroneous datetime handling during DST transition...")
+                data = _get_json(link)
+                if not data:
                     continue
 
-                if eTime >= datetime.datetime(2019,3,31,1,0,0) and eTime <= datetime.datetime(2019,3,31,4,0,0):
-                    logging.info("[VODAFONE] Skipping due to erroneous datetime handling during DST transition...")
-                    continue
+                for obj in data.get("result", {}).get("objects", []):
+                    pid = obj.get("id") or obj.get("crid")
+                    if pid is not None and pid in seen:
+                        continue
+                    seen.add(pid)
 
-                #sTime = tz.localize(sTime)
-                #sTime = sTime.astimezone(tz)
-                #eTime = tz.localize(eTime)
-                #eTime = eTime.astimezone(tz)
+                    for programme in _build_programmes(obj, channelInfos):
+                        xmltv.addProgramme(programme)
+                        got += 1
 
-                sTime = tz.fromutc(sTime)
-                eTime = tz.fromutc(eTime)
+        if got == 0:
+            logging.warning("[VODAFONE] %s - no programmes returned.", site_id)
 
-                sTime = sTime.strftime("%Y%m%d%H%M%S %z")
-                eTime = eTime.strftime("%Y%m%d%H%M%S %z")
-                title = program["fullTitle"]
-                desc = program["description"]
-                iconSrc = program["image"]
-                for channelInfo in item.getChannelList():
-                    p = Programme(channelInfo.getId(), sTime, eTime, title, desc, "pt", iconSrc)
-                    _findSeasonEpisode(title,p)
-                    xmltv.addProgramme(p)
-
-       
     return xmltv
 
-def _findSeasonEpisode(title,p):
-    m = re.match(r'.*(\s+|:)T(\d+)(?:\s+-?\s*Ep.\s*)?(\d+)?$', title)
-    if m:
-        season = m.group(2)
-        if season:
-            p.setSeasonNumber(season)
-        episode = m.group(3)
-        if episode:
-            p.setEpisodeNumber(episode)
-    else:
-        m = re.match(r'.*\s+Ep.\s*(\d+)$', title)
-        if m:
-            episode = m.group(1)
-            if episode:
-                p.setEpisodeNumber(episode)
 
-def _getSupportedChannels():
+def _build_programmes(obj, channelInfos):
 
-    url = "https://web.ott-red.vodafone.pt/ott3_webapp/v1/channels"
-
-    channels = set()
-
-    content = _get_content(url)
-    if content != None and content.getcode() != 200: return None
-    myfile = content.read().decode('utf8')
     try:
-        myfile = json.loads(myfile)
-    except ValueError:
-        return None
+        start = int(obj["startDate"])
+        end = int(obj["endDate"])
+    except (KeyError, ValueError, TypeError):
+        return []
 
-    for channel in myfile["data"]:
-        channels.add(channel["id"])
+    sTime = datetime.datetime.fromtimestamp(start, tz).strftime("%Y%m%d%H%M%S %z")
+    eTime = datetime.datetime.fromtimestamp(end, tz).strftime("%Y%m%d%H%M%S %z")
 
-    return channels
+    title = obj.get("name", "")
+    desc = obj.get("description", "")
 
-def _get_content(url):
+    iconSrc = None
+    images = obj.get("images") or []
+    if images:
+        iconSrc = images[0].get("url")
 
-    content = None
-    count = 1
-    while count <= 5:
+    season = _meta(obj, "season number")
+
+    programmes = []
+    for channelInfo in channelInfos:
+        p = Programme(channelInfo.getId(), sTime, eTime, title, desc, "pt", iconSrc)
+        if season and season.isdigit() and int(season) > 0:
+            p.setSeasonNumber(season)
+        programmes.append(p)
+
+    return programmes
+
+
+def _meta(obj, key):
+    meta = obj.get("metas", {}).get(key)
+    if meta:
+        return meta.get("value")
+    return None
+
+
+def _get_json(url):
+
+    req = urllib.request.Request(url, headers=HEADERS)
+
+    for attempt in range(1, 4):
         try:
-            content = urllib.request.urlopen(url)
-            break
-        except HTTPError:
-            ...
-            
-        count += 1
+            with urllib.request.urlopen(req, timeout=30) as resp:
+                if resp.getcode() != 200:
+                    return None
+                return json.loads(resp.read().decode("utf-8"))
+        except HTTPError as e:
+            if e.code == 404:
+                return None  # no listing for this day/slot
+            logging.debug("[VODAFONE] HTTP %s for %s (attempt %d)", e.code, url, attempt)
+        except (URLError, ValueError) as e:
+            logging.debug("[VODAFONE] error %s for %s (attempt %d)", e, url, attempt)
         time.sleep(1)
 
-    if count > 5:
-        logging.warning("After 5 attempts we couldn't retrieve the information for this channel." )
-
-    return content
+    logging.warning("[VODAFONE] failed after retries: %s", url)
+    return None
